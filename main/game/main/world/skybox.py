f4e33266d24f69a7dfc0f123b95dfc5c01285385
Lixt company <60551627+luis605@users.noreply.github.com>
2022-11-18 19:11:52 +0000

    Delete main directory

--- main/game/main/world/skybox.py
+++
@@ -1,7 +0,0 @@
-from ursina import *
-
-class Skybox(Entity):
-    def __init__(self):
-        # Sky
-        self.sky_texture = load_texture("assets/skybox-4k.jpg")
-        Sky(texture=self.sky_texture)
