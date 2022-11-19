19121d03bd0c890011459ae8760c740fe3301ef8
Lixt company <60551627+luis605@users.noreply.github.com>
2022-11-19 17:40:10 +0000

    Update install.sh

--- main/build/install.sh
+++ main/build/install.sh
@@ -1,3 +1,5 @@
 sudo apt-get install python3.11-dev || sudo yum install python3-devel || sudo dnf install python3-devel || sudo zypper in python3-devel || sudo apk add python3-dev || apt-cyg install python3-devel
 
-pip3 install -r 'requirements.txt'
+sudo apt-get install python3-pip
+
+sudo python3.11 -m pip install -r 'requirements.txt' --upgrade
